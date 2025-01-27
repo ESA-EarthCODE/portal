36e0e8d4b59c2ba6f95825ed0720a544d342d34f
silvester-pari <silvester.pari@eox.at>
2025-01-27 17:07:10 +0100

    chore(TEMP): remove login button

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -33,7 +33,6 @@ export default {
         text: "Contact",
       },
     ],
-    login: "https://dashboard.earthcode-testing.earthcode.eox.at/"
   },
   head: [
     ["link", { rel: "icon", href: "/img/EarthCODE_Favicon_32x32px.png" }],
